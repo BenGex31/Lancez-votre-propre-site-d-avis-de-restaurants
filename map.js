8587dbc4116c0f2b7b5cc293550e229629bcdf32
[Benjamin Desole] <[benjamin.desole@gmail.com]>
2020-08-25 19:03:11 +0200

    Affichage carte sur la page web

--- map.js
+++ map.js
@@ -1,10 +1,18 @@
 class Map {
-    constructor(latitude, longitude) {
-        this.latitude = latitude;
-        this.longitude = longitude;
-    }
-
-    initMap() {
-        
+    constructor(lat, lng) {
+        this.lat = lat;
+        this.lng = lng;
     }
 }
+
+let parisLocation = {lat : 48.8566969, lng: 2.3514616};
+
+let map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: parisLocation
+});
+
+let marker = new google.maps.Marker({
+    position : parisLocation,
+    map: map
+});

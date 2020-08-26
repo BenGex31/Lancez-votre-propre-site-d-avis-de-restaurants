46e276f0065197bef78c84c09e869e0cee0550d9
[Benjamin Desole] <[benjamin.desole@gmail.com]>
2020-08-26 12:00:35 +0200

    initMap orientée objet

--- map.js
+++ map.js
@@ -2,22 +2,25 @@ class Map {
     constructor(lat, lng) {
         this.lat = lat;
         this.lng = lng;
+        this.zoom = 10;
+        this.location = {lat: this.lat, lng: this.lng};
+    }
+    
+    initMap() {
+        let city = this.location;
+
+        let map = new google.maps.Map(
+            document.getElementById('map'), {
+                zoom: this.zoom,
+                center: city
+        });
+        
+        let marker = new google.maps.Marker({
+            position: city,
+            map: map
+        });
     }
 }
 
-// Initialize and add the map
-function initMap() {
-    // The location of Uluru
-    let paris = {lat: 48.8566969, lng: 2.3514616};
-    // The map, centered at Uluru
-    let map = new google.maps.Map(
-        document.getElementById('map'), {
-            zoom: 12,
-            center: paris
-        });
-    // The marker, positioned at Uluru
-    let marker = new google.maps.Marker({
-        position: paris,
-        map: map
-    });
-  }
+const paris = new Map(48.8566969, 2.3514616);
+paris.initMap();

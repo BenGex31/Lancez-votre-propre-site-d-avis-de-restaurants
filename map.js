8796e9244d5a284fa3a8a875581423c34e2d4d3d
[Benjamin Desole] <[benjamin.desole@gmail.com]>
2020-08-26 10:25:34 +0200

    map ajoutée sans marker

--- map.js
+++ map.js
@@ -5,14 +5,19 @@ class Map {
     }
 }
 
-let parisLocation = {lat : 48.8566969, lng: 2.3514616};
-
-let map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: parisLocation
-});
-
-let marker = new google.maps.Marker({
-    position : parisLocation,
-    map: map
-});
+// Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    let paris = {lat: 48.8566969, lng: 2.3514616};
+    // The map, centered at Uluru
+    let map = new google.maps.Map(
+        document.getElementById('map'), {
+            zoom: 12,
+            center: paris
+        });
+    // The marker, positioned at Uluru
+    let marker = new google.maps.Marker({
+        position: paris,
+        map: map
+    });
+  }

1bbf288643ac38c3cb68b6b6ff67d5ca6fedf330
[Benjamin Desole] <[benjamin.desole@gmail.com]>
2020-08-28 18:19:04 +0200

    Map affichée avec restos (icones perso) + géolocalisation utilisateur
    POO à mettre en place

--- main.js
+++ main.js
@@ -1,5 +1,73 @@
-window.onload = function() {
+/*window.onload = function() {
     
-};
+}*/
 
-$('<i></i>').prependTo($('.restaurantAddress')).addClass("fas fa-map-marker-alt").css({"margin-right" : "5px", "color" : "#eb2f06"});
+    initMap = () => {
+    // The location of Paris
+    let paris = {lat: 48.8565387, lng: 2.3518054};
+    // The map, centered at Paris
+    let map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 12, center: paris});
+    // The marker, positioned at Uluru
+    let markerParis = new google.maps.Marker({position: paris, map: map, label: "paris"});
+
+    for(let restaurant of restaurants) {
+        let markerRestaurant = new google.maps.Marker({
+            position: {lat: restaurant.lat, lng: restaurant.long},
+            map: map,
+            draggable: true,
+            animation: google.maps.Animation.DROP,
+            label: restaurant.restaurantName,
+            icon: {
+                url: "img/icon-restaurant-location.png",
+                scaledSize: new google.maps.Size(50, 50),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(0, 0)
+            }
+        });
+    }
+
+    let infoWindow = new google.maps.InfoWindow;
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+        let pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        };
+
+        let markerUser = new google.maps.Marker({
+            position: pos,
+            map: map,
+            animation: google.maps.Animation.DROP,
+            label : "Vous êtes ici !",
+            icon: {
+                url: "img/icon-user-location.png",
+                scaledSize: new google.maps.Size(50, 50),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(0, 0)
+            }
+        })
+
+        map.setCenter(pos);
+        }, function() {
+        handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+                            'Erreur: Le service de géolocalisation a échoué.' :
+                            'Error: Votre navigateur ne prend pas en charge la géolocalisation.');
+    infoWindow.open(map);
+    }
+}
+
+$(function(){
+    $('<i></i>').prependTo($('.restaurantAddress')).addClass("fas fa-map-marker-alt").css({"margin-right" : "5px", "color" : "#eb2f06"});
+});
--- map.js
+++ map.js
@@ -1,26 +1,6 @@
-class Map {
+/*class Map {
     constructor(lat, long) {
         this.lat = lat;
         this.long = long;
-        this.zoom = 12;
-        this.location = {lat: this.lat, lng: this.long};
     }
-    
-    initMap() {
-        let city = this.location;
-
-        let map = new google.maps.Map(
-            document.getElementById('map'), {
-                zoom: this.zoom,
-                center: city
-        });
-        
-        let marker = new google.maps.Marker({
-            position: city,
-            map: map
-        });
-    }
-}
-
-const paris = new Map(48.8566969, 2.3514616);
-paris.initMap();
+}*/

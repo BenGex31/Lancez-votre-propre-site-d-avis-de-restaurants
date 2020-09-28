70996df70567367a343e1c0ef24f810085517c53
[Benjamin Desole] <[benjamin.desole@gmail.com]>
2020-09-28 11:09:35 +0200

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ class App {
 
         const map = new MyMap(48.8565387, 2.3518054);
         map.createMap();
-        console.log(map);
+        //console.log(map);
 
         const filter = new Restaurant();
         

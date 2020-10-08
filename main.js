5f1e95b23e289352e32b6bc4df1e29ad90c49684
[Benjamin Desole] <[benjamin.desole@gmail.com]>
2020-10-08 18:55:44 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 window.onload = function() {
-  const app = new App();
-  app.filterRestaurants();
+  /*const app = new App();
+  app.filterRestaurants();*/
+
+  const map = new MyMap(48.8565387, 2.3518054);
+        map.createMap();
+        console.log(map);
 }
 
 $(function() {

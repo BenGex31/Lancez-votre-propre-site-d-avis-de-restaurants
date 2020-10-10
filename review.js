0998696a757f14bc269fd4dd3d96ec2d202180b1
[Benjamin Desole] <[benjamin.desole@gmail.com]>
2020-10-10 14:59:32 +0200

    Update review.js

--- review.js
+++ review.js
@@ -1,8 +1,10 @@
 class Review {
-    constructor(author, stars, comment, place_id) {
+    constructor(author, author_profil_picture, stars, comment, place_id, relative_time_description) {
         this.author = author;
+        this.author_profil_picture = author_profil_picture
         this.stars = stars;
         this.comment = comment;
         this.place_id = place_id;
+        this.relative_time_description = relative_time_description;
     }
 }

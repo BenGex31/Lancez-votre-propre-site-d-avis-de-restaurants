52531854f1e71b2265807707fe43f0ba3f290f21
[Benjamin Desole] <[benjamin.desole@gmail.com]>
2020-10-15 17:19:55 +0200

    Update review.js

--- js/review.js
+++ js/review.js
@@ -1,4 +1,16 @@
+/**
+ * Class representing a review
+ */
 class Review {
+    /**
+     * 
+     * @param {string} author 
+     * @param {image} author_profil_picture 
+     * @param {number} stars 
+     * @param {string} comment 
+     * @param {string} place_id 
+     * @param {string} relative_time_description 
+     */
     constructor(author, author_profil_picture, stars, comment, place_id, relative_time_description) {
         this.author = author;
         this.author_profil_picture = author_profil_picture
